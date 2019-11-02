58f3da0e70998550165e15ad4d89d6a372924749
joey <joey.douglas@gmail.com>
2019-11-02 11:33:53 -0500

    created simple http server that runs different HandleFuncs based on an env.

---
+++ README.md
@@ -0,0 +1,2 @@
+#### Go Container Example
+This is a simple Go http server demonstrating the use of containers for multi-environment deployments and multi-stage builds. 
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := ":8886"
+	enviro := os.Getenv("APP_ENV")
+
+	switch enviro {
+	case "dev":
+		http.HandleFunc("/", devFunction)
+		fmt.Println("Environment set to " + enviro)
+	case "prod":
+		http.HandleFunc("/", prodFunction)
+		fmt.Println("Environment set to " + enviro)
+	default:
+		fmt.Println("No environment specified. Closing server.")
+		return
+
+	}
+	fmt.Println("Server listening on port " + port)
+	http.ListenAndServe(port, nil)
+
+}
+
+func devFunction(w http.ResponseWriter, r *http.Request) {
+	appEnv := "This is DEV."
+	fmt.Fprint(w, "", appEnv)
+}
+
+func prodFunction(w http.ResponseWriter, r *http.Request) {
+	appEnv := "This is PROD."
+	fmt.Fprint(w, "", appEnv)
+}
